2aeb43b63b8a9ad4c6660dbce12314e1de254f05
restitution <sniffjoke@gmail.com>
2025-07-28 00:35:11 +0200

    forbid login for banned user

--- src/features/users/infrastructure/users.repository.to.ts
+++ src/features/users/infrastructure/users.repository.to.ts
@@ -1,22 +1,33 @@
-import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreateUserDto, EmailConfirmationModel } from '../api/models/input/create-user.dto';
+import {
+  CreateUserDto,
+  EmailConfirmationModel,
+} from '../api/models/input/create-user.dto';
 import { UserEntity } from '../domain/user.entity';
 import { EmailConfirmationEntity } from '../domain/email-confirmation.entity';
 import { UserScoreEntity } from '../../quiz/domain/user-score.entity';
 import { BanInfoEntity } from '../domain/ban-info.entity';
 
-
 @Injectable()
 export class UsersRepositoryTO {
   constructor(
-    @InjectRepository(UserEntity) private readonly uRepository: Repository<UserEntity>,
-    @InjectRepository(EmailConfirmationEntity) private readonly eCRepository: Repository<EmailConfirmationEntity>,
-  ) {
-  }
+    @InjectRepository(UserEntity)
+    private readonly uRepository: Repository<UserEntity>,
+    @InjectRepository(EmailConfirmationEntity)
+    private readonly eCRepository: Repository<EmailConfirmationEntity>,
+  ) {}
 
-  async createUser(userData: CreateUserDto, emailConfirmationDto: EmailConfirmationModel) {
+  async createUser(
+    userData: CreateUserDto,
+    emailConfirmationDto: EmailConfirmationModel,
+  ) {
     const user = new UserEntity();
     user.login = userData.login;
     user.email = userData.email;
@@ -25,14 +36,16 @@ export class UsersRepositoryTO {
 
     const emailConfirmation = new EmailConfirmationEntity();
     emailConfirmation.userId = newUser.id;
-    emailConfirmation.confirmationCode = emailConfirmationDto.confirmationCode as string;
-    emailConfirmation.expirationDate = emailConfirmationDto.expirationDate as string;
+    emailConfirmation.confirmationCode =
+      emailConfirmationDto.confirmationCode as string;
+    emailConfirmation.expirationDate =
+      emailConfirmationDto.expirationDate as string;
     emailConfirmation.isConfirm = emailConfirmationDto.isConfirm;
 
     const banInfo = new BanInfoEntity();
     banInfo.userId = newUser.id;
 
-    const userScore = new UserScoreEntity()
+    const userScore = new UserScoreEntity();
     userScore.userId = newUser.id;
 
     newUser.score = userScore;
@@ -44,18 +57,15 @@ export class UsersRepositoryTO {
   }
 
   async findUserById(id: string) {
-    const findedUser = await this.uRepository.findOne(
-      {
-        where: { id },
-        relations: ['banInfo'],
-      },
-    );
+    const findedUser = await this.uRepository.findOne({
+      where: { id },
+      relations: ['banInfo'],
+    });
     if (!findedUser) {
       throw new NotFoundException('User not found');
     }
     return findedUser;
   }
-
 
   async updateUserByActivateEmail(userId: any) {
     const findedEmailInfo = await this.eCRepository.findOne({
@@ -69,14 +79,19 @@ export class UsersRepositoryTO {
     return null;
   }
 
-  async updateUserByResendEmail(userId: string, emailConfirmation: EmailConfirmationModel) {
+  async updateUserByResendEmail(
+    userId: string,
+    emailConfirmation: EmailConfirmationModel,
+  ) {
     const findedEmailInfo = await this.eCRepository.findOne({
       where: { userId },
     });
     if (findedEmailInfo) {
       findedEmailInfo.isConfirm = emailConfirmation.isConfirm;
-      findedEmailInfo.confirmationCode = emailConfirmation.confirmationCode as string;
-      findedEmailInfo.expirationDate = emailConfirmation.expirationDate as string;
+      findedEmailInfo.confirmationCode =
+        emailConfirmation.confirmationCode as string;
+      findedEmailInfo.expirationDate =
+        emailConfirmation.expirationDate as string;
       await this.eCRepository.manager.save(findedEmailInfo);
       return findedEmailInfo;
     }
@@ -84,18 +99,17 @@ export class UsersRepositoryTO {
   }
 
   async findUserByIdOrNull(id: string) {
-    const findedUser = await this.uRepository.findOne(
-      { where: { id } },
-    );
+    const findedUser = await this.uRepository.findOne({ where: { id } });
     if (!findedUser) {
       return null;
     } else return findedUser;
   }
 
   async findUserByLogin(login: string) {
-    const findedUser = await this.uRepository.findOne(
-      { where: { login } },
-    );
+    const findedUser = await this.uRepository.findOne({
+      where: { login },
+      relations: ['banInfo'],
+    });
     if (!findedUser || findedUser?.banInfo.isBanned) {
       throw new UnauthorizedException('User not found');
     }
@@ -105,9 +119,8 @@ export class UsersRepositoryTO {
 
   async findUserByEmail(email: string) {
     const findedUser = await this.uRepository.findOne({
-        where: { email },
-      },
-    );
+      where: { email },
+    });
     if (!findedUser) {
       throw new BadRequestException('Email not exists');
     }
@@ -117,32 +130,28 @@ export class UsersRepositoryTO {
   async findUserByCode(code: string) {
     const findEmailInfo = await this.eCRepository.findOne({
       where: { confirmationCode: code },
-    })
+    });
     if (!findEmailInfo) {
-        throw new BadRequestException('Code not found');
+      throw new BadRequestException('Code not found');
     }
     return findEmailInfo;
   }
 
   async deleteUserById(id: string) {
     const findedUser = await this.findUserById(id);
-    return await this.uRepository.delete(
-      { id },
-    );
+    return await this.uRepository.delete({ id });
   }
 
   async checkIsUserExists(login: string, email: string) {
-    const findedUserByLogin = await this.uRepository.findOne(
-      { where: { login } },
-    );
+    const findedUserByLogin = await this.uRepository.findOne({
+      where: { login },
+    });
     if (findedUserByLogin) {
-      throw new BadRequestException(
-        'Login already exists',
-      );
+      throw new BadRequestException('Login already exists');
     }
-    const findedUserByEmail = await this.uRepository.findOne(
-      { where: { email } },
-    );
+    const findedUserByEmail = await this.uRepository.findOne({
+      where: { email },
+    });
     if (findedUserByEmail) {
       throw new BadRequestException('Email already exists');
     }
@@ -153,13 +162,12 @@ export class UsersRepositoryTO {
       where: { userId },
     });
     if (!findedEmailInfo) {
-      throw new BadRequestException('User profile not found')
+      throw new BadRequestException('User profile not found');
     }
     return findedEmailInfo;
   }
 
   async saveUser(user: UserEntity) {
-    return await this.uRepository.save(user)
+    return await this.uRepository.save(user);
   }
-
 }
